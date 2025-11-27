3306b81120e46a148787856067262803492371de
Isaias Lourenco <moerbius@gmail.com>
2025-11-27 13:49:58 +0000

    Create Sprite.hpp

---
+++ src/systems/Sprite.hpp
@@ -0,0 +1,23 @@
+#ifndef SPRITE_HPP
+#define SPRITE_HPP
+
+#include "../main.hpp"
+
+class Sprite {
+public:
+    Sprite();
+    Sprite(SDL_Texture* texture, int spriteId);
+    
+    void setSpriteId(int spriteId);
+    void render(SDL_Renderer* renderer, int x, int y);
+    void render(SDL_Renderer* renderer, int x, int y, int width, int height);
+    
+private:
+    SDL_Texture* texture;
+    SDL_Rect srcRect;
+    
+    static const int SPRITE_SIZE = 32;     // Changed from 16 to 32
+    static const int SPRITES_PER_ROW = 16;
+};
+
+#endif // SPRITE_HPP
