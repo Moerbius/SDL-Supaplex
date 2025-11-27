42073803da81e31bbd06f88f5ae6730531dba992
Isaias Lourenco <moerbius@gmail.com>
2025-11-27 23:14:44 +0000

    Update main.hpp

--- src/main.hpp
+++ src/main.hpp
@@ -25,21 +25,49 @@ enum class GameState {
 // Tile types for Supaplex
 enum class TileType {
     EMPTY,
-    WALL,
-    BASE,
-    INFOTRON,
-    CHIP,
-    HARDWARE,
     PLAYER,
-    ZONK,
-    DISK,
-    PORT,
-    SNIK_SNAK,
-    YELLOW_DISK,
+    INFOTRON,
     TERMINAL,
+    BASE,
+    
+    EXIT,
+
+    BUG,
+    SNIK_SNAK,
+    ZONK,
+
+    DISK_ORANGE,
+    DISK_YELLOW,
+    DISK_RED,
+
+    PORT_1,
+    PORT_2,
+    PORT_3,
+
+
+    CHIP_1,
+    CHIP_2,
+    CHIP_3,
+
+    HARDWARE_1,
+    HARDWARE_2,
+    HARDWARE_3,
+    HARDWARE_4,
+    HARDWARE_5,
+    HARDWARE_6,
+    HARDWARE_7,
+    HARDWARE_8,
+    HARDWARE_9,
+    HARDWARE_10
+    
+    /* DISK,
+    PORT,
+    
+    YELLOW_DISK,
+    
     RED_DISK,
-    ELECTRON,
-    BUG
+    ELECTRON */
+    
 };
 
 #endif // MAIN_HPP
